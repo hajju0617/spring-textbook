d366183480ac1d4f20c0dde2208b14e0d165d5ed
hajju0617 <joowon023617@gmail.com>
2025-12-16 21:27:56 +0900

    Spring bean definition

---
+++ src/main/java/com/project/ch2/definition_02/Parrot.java
@@ -0,0 +1,12 @@
+package com.project.ch2.definition_02;
+
+public class Parrot {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
